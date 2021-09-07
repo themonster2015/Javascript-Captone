fb3730d2c39d43b686bd2debb88a362f0767fa2d
Oguadinma Nkiruka Ngozika <nkirukaogu@gmail.com>
2021-09-07 15:48:04 +0100

    Revert "Like action"

--- src/loadPoke.js
+++ src/loadPoke.js
@@ -1,55 +1,28 @@
-import { postLike } from './fetchInteractions';
-
 const list = document.getElementsByClassName('list');
 
 const loadPoke = (data, likes) => {
   data.forEach((poke, index) => {
     const wrapper = document.createElement('div');
-    wrapper.classList.add('card');
     const img = document.createElement('img');
     const description = document.createElement('div');
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
     description.classList.add('description');
     const name = document.createElement('span');
-    name.classList.add('card-title');
-    name.innerHTML = poke.name.slice(0, 1).toUpperCase() + poke.name.slice(1);
+    name.innerHTML = poke.name;
     const likeDiv = document.createElement('div');
-    likeDiv.classList.add('d-flex', 'flex-column');
-    let likeCount;
-    const item = likes.filter((i) => i.item_id === poke.name);
-    if (item.length === 0) {
-      likeCount = 0;
-    } else {
-      likeCount = item[0].likes;
-    }
-    const likeBtn = document.createElement('btn');
-    likeBtn.classList.add('btn');
-    likeBtn.innerHTML += "<i class='fa fa-heart' aria-hidden='true'></i>";
-    likeBtn.addEventListener('click', () => {
-      postLike(poke.name).then(() => {
-        window.location.reload();
-      });
-    });
-
-    likeDiv.appendChild(likeBtn);
-    const likeCounter = document.createElement('span');
-    likeCounter.innerHTML = `${likeCount} likes`;
+    likeDiv.innerHTML = `<i class='fa fa-heart' aria-hidden='true'></i><span>${likes[0].likes} likes</span>`;
     wrapper.classList.add('pokeWrapper');
-    likeDiv.appendChild(likeCounter);
     img.setAttribute('src', `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png`);
     img.setAttribute('alt', 'Pokemon');
-    img.classList.add('pokeImg', 'card-img-top');
+    img.classList.add('pokeImg');
     const btn = document.createElement('btn');
-    btn.classList.add('cmtBtn', 'btn', 'btn-outline-dark', 'btn-lg');
+    btn.classList.add('cmtBtn');
     btn.innerText = 'Comments';
 
     wrapper.appendChild(img);
     description.appendChild(name);
     description.appendChild(likeDiv);
-    cardBody.appendChild(description);
-    cardBody.appendChild(btn);
-    wrapper.appendChild(cardBody);
+    wrapper.appendChild(description);
+    wrapper.appendChild(btn);
     list[0].appendChild(wrapper);
   });
 };
