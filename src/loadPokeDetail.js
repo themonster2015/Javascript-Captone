60796d8b5f93db06d9494064c1e40ac2bc6b2f40
enkog <nkirukaogu@gmail.com>
2021-09-08 11:22:31 +0100

    Display comments and comment counts on modal window

--- src/loadPokeDetail.js
+++ src/loadPokeDetail.js
@@ -8,6 +8,8 @@ const countComments = (comments) => (comments.length > 0 ? comments.length : 0);
 const loadPokeDetail = async (imgUrl, pokeName, index) => {
   index += 1;
   const stats = await fetchPokeStats(index);
+  const comments = await fetchComments(index);
+  const commentsCount = countComments(comments);
 
   const html = `
   <article class="d-flex flex-column justify-content-center">
@@ -26,9 +28,28 @@ const loadPokeDetail = async (imgUrl, pokeName, index) => {
         <p class="pokemon__row"><span>Order:</span>${stats.order}</p>
       </div>
     </div>
+    <div class="comment-form my-3">
+        <h4 class="text-center text-primary">Comments(${commentsCount})</h4>
+        <div id="comments"></div>
+    </div>
+  </article>
+
   `;
   commentModalDiv.insertAdjacentHTML('beforeend', html);
   commentModalDiv.classList.remove('hidden');
+
+  const commentsDiv = document.getElementById('comments');
+
+  if (commentsCount > 0) {
+    comments.forEach((comment) => {
+      const paragraph = document.createElement('p');
+      paragraph.innerHTML = `<span> ${comment.creation_date} </span>
+                       <span> ${comment.username}: </span>
+                       <span> ${comment.comment} </span>`;
+
+      commentsDiv.appendChild(paragraph);
+    });
+  }
 
   const btnCloseModal = document.querySelector('.close-modal');
   const closeModal = () => {
