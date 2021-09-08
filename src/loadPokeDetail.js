126d201ef4b0ae33f6231b1c614d34650f5f8584
enkog <nkirukaogu@gmail.com>
2021-09-08 12:00:50 +0100

    Add new comment form on the comments pop up

--- src/loadPokeDetail.js
+++ src/loadPokeDetail.js
@@ -20,17 +20,25 @@ const loadPokeDetail = async (imgUrl, pokeName, index) => {
     <div class="pokemon__data">
       <h3 class="pokemon__name text-center mt-3 text-primary">${pokeName}</h3>
       <div class="d-flex justify-content-between">
-        <p class="pokemon__row"><span>Height:</span>${stats.height}</p>
-        <p class="pokemon__row"><span>Moves:</span>${stats.moves}</p>
+        <p class="pokemon__row"><span class="badge bg-primary">Height:</span>${stats.height}</p>
+        <p class="pokemon__row"><span class="badge bg-primary">Moves:</span>${stats.moves}</p>
       </div>
       <div class="d-flex justify-content-between">
-        <p class="pokemon__row"><span>Weight:</span>${stats.weight}</p>
-        <p class="pokemon__row"><span>Order:</span>${stats.order}</p>
+        <p class="pokemon__row"><span class="badge bg-primary mr-5">Weight:</span>${stats.weight}</p>
+        <p class="pokemon__row"><span class="badge bg-primary">Order:</span>${stats.order}</p>
       </div>
     </div>
-    <div class="comment-form my-3">
-        <h4 class="text-center text-primary">Comments(${commentsCount})</h4>
+    <div class="comment-form my-2">
+        <h5 class="text-center text-primary">Comments(${commentsCount})</h5>
         <div id="comments"></div>
+        <h5 class="text-center text-primary mt-2">Add a comment</h5>
+        <div class="mb-3">
+          <input type="text" class="form-control name-input" placeholder="Your name">
+        </div>
+        <div class="mb-3">
+          <textarea class="form-control comment-input" rows="3" placeholder="Your insights"></textarea>
+        </div>
+        <button type="submit" class="add btn btn-primary">Comment</button>
     </div>
   </article>
 
