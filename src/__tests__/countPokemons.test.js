5112e5ac8a3be44db924f1657525b6680be67ca5
themonster2015 <quynhyen.vothi@gmail.com>
2021-09-09 10:13:13 +0200

    wrote tests for pokemon counter

--- src/__tests__/countPokemons.test.js
+++ src/__tests__/countPokemons.test.js
@@ -1,7 +1,20 @@
 import countPokemons from '../countPokemons';
-import fetchPoke from '../fetchPoke'
 
-it('should return the correct number of pokemons', () => {
-    fetchPoke.mockImplementation(() => Promise.resolve(["Pokemon 1", "Pokemon 2", "Pokemon 3"]))
-const pokemons = 
+const pokemons = [
+  { name: 'bulbasaur', url: 'https://example.com/1/' },
+  { name: 'ivysaur', url: 'https://example.com/2/' },
+  { name: 'venusaur', url: 'https://example.com/3/' },
+  { name: 'charmander', url: 'https://example.com/4/' },
+  { name: 'charmeleon', url: 'https://example.com/5/' },
+  { name: 'charizard', url: 'https://example.com/6/' },
+];
+
+describe('Count pokemons', () => {
+  test('that it return the correct number of pokemons', () => {
+    expect(countPokemons(pokemons)).toEqual(6);
+  });
+
+  test('that it should return 0 if passed an empty array', () => {
+    expect(countPokemons([])).toEqual(0);
+  });
 });
